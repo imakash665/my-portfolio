d4701480b4f21c41c479b3558d908718d1b4f00a
imakash665 <imakash665@gmail.com>
2026-03-29 03:52:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Portfolio Website
+This is my personal Portfolio website
+
+[Link to My Portfolio](https://theakash.xyz/)
+
+
---
+++ js/contact_me.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const sendMessageButton = document.getElementById("sendMessageButton");
+
+  sendMessageButton.addEventListener("click", function () {
+    const form = document.getElementById("contactForm");
+    const formData = new FormData(form);
+
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const subject = formData.get("subject");
+    const message = formData.get("message");
+
+    // Add the name at the end of the main message
+    const fullMessage = `${message}\n\n- ${name}\n\n- ${email}`;
+
+    const mailtoLink = `mailto:imakash665@gmail.com?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(fullMessage)}`;
+    
+    window.location.href = mailtoLink;
+  });
+});
+
+
+
